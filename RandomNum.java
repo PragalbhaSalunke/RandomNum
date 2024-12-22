ee173cbeb1bc64f09b800a29f6862efb5273de44
PragalbhaSalunke <salunkepragalbha99@gmail.com>
2024-12-22 13:43:05 +0530

    RandomNumber

---
+++ RandomNum.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class RandomNum {
+    public static void main(String[] args) {
+       
+        Random rand = new Random();
+
+        for (int i = 0; i < 5; i++) {
+            
+            int randomNumber = rand.nextInt(9000) + 1000;
+            System.out.println(randomNumber);
+        }
+    }
+}
